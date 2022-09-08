274c486d9a4b615d9b5b2e973c000669804cedd2
Jason Uzoukwu <theweirdjason@gmail.com>
2022-09-08 03:33:15 -0700

    stuff

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * Main - yeah
+ * main - yeah
  * Return: uhhh
  */
 int main(void)
@@ -8,4 +8,3 @@ int main(void)
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-	
