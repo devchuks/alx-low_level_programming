53c8640fd0cc09c78b4a26d3e02c98e3da90b377
Jason Uzoukwu <theweirdjason@gmail.com>
2022-09-09 09:23:07 -0700

    jesus christ man

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
- /**
- * main - stuff stuff
+/**
+ * main - entry point
  * Return: always
  */
 int main(void)
 {
 	int a;
 
-	for (a = 0; a <= 10; a++)
+	for (a = 0; a < 10; a++)
 		printf("%d", a);
 	putchar ('\n');
 	return (0);
