901fc01c95981e975647f1b8fd98e1bb0329a1d3
Jason Uzoukwu <theweirdjason@gmail.com>
2022-09-09 11:00:02 -0700

    wahoo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,7 +16,7 @@ int main(void)
 			{
 				putchar((i % 10) + '0');
 				putchar((j % 10) + '0');
-				if (i > 20 || j > 19)
+				if (i != 20 || j != 19)
 				{
 					putchar(',');
 					putchar(' ');
