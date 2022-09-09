cc0e6be856da492e6934277084af668b811d6f65
Jason Uzoukwu <theweirdjason@gmail.com>
2022-09-09 10:52:34 -0700

    aaaaaah

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 10; i < 20; i++)
+	{
+		for (j = 10; j < 20; i++)
+		{
+			if ((j % 10) > (i % 10))
+			{
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+				if (i > 20 || j > 19)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
