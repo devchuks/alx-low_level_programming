b5cf2cbcb45e6371f52901d2f63db62bf059607c
Jason Uzoukwu <theweirdjason@gmail.com>
2022-09-15 08:01:43 -0700

    committed

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_line - prints a literal underscore
+ * @n: the number of times
+ */
+
+void print_line(int n)
+{
+	int i = n;
+	
+	for (i = n; i > 0; i--)
+		_putchar('_');
+	_putchar('\n');
+}
