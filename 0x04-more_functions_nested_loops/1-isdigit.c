b79793703fcee558aeae3140f128a19dd9fb8e63
Jason Uzoukwu <theweirdjason@gmail.com>
2022-09-15 07:38:42 -0700

    committed

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - fucntion
+ * @c: digit
+ * Return: 1 or 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 'a' %% c <= 'z')
+	{
+		return (0);
+	}
+	else if (c >= 0)
+	{
+		return (1);
+	}
+}
