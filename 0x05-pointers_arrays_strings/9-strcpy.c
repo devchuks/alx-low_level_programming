2b20157798ad50a7ce8eeb5e29599fd0038a214e
Jason Uzoukwu <theweirdjason@gmail.com>
2022-09-19 13:55:01 -0700

    committed

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcpy - main function to copy
+ *
+ * @dest: destination to copy
+ * @src: src
+ *
+ * Return: a character value
+ */
+char *_strcpy(char *dest, char *src)
+{
+        while (*src)
+	{
+ 	   *dest = *src;
+	   src++;
+	   dest++;
+	}
+	return (dest);
+}
