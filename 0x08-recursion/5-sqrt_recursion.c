14848dca5d55b73d1e51b1f15e47d89fd757da8b
Jason Uzoukwu <theweirdjason@gmail.com>
2022-09-28 11:48:40 -0700

    committed

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,34 +1,31 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
  * find_root - find square root of n, starting from the smallest possible, 0
- * @number: n
- * @sqroot: test this root
+ * @n: n
+ * @root: test this root
  * Return: natural square root, or -1 if not natural root
  */
 
-int find_root(int number, int sqroot)
+int find_root(int n, int root)
 {
-	if (sqroot * sqroot > number)
+	if (root * root > n)
 		return (-1);
 
-	if (sqroot * sqroot == number)
-		return (sqroot);
+	if (root * root == n)
+		return (root);
 
-	return (find_root(number, sqroot + 1));
+	return (find_root(n, root + 1));
 }
 
 /**
  * _sqrt_recursion - find natural square root of n
  * @n: n
- * @number: n
  * Return: natural square root, or -1 if not natural root
  */
 
 int _sqrt_recursion(int n)
 {
-	number = n;
-
 	if (n < 0)
 		return (-1);
 
