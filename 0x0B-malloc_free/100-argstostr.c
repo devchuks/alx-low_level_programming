82e235b616b7b4c3715f028a99a2a4f5b4fc1fdc
Jason Uzoukwu <theweirdjason@gmail.com>
2022-10-03 09:23:15 -0700

    committed

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,46 +1,63 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
 /**
- * argstostr - gives array of args
- * @ac: argument count
- * @av: array of arguments
- * Return: char value
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
  */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+		len++, s++;
+
+	return (len);
+}
+
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: argc
+ * @av: arguments
+ * Return: pointer to array
+ */
+
 char *argstostr(int ac, char **av)
 {
-	int size;
 	char *s;
-	int i;
-	int j;
-	int k;
+	int len = 0, i, j, k = 0;
 
-	if (ac == 0 || av == NULL)
+	if (ac == 0 || av == NULL) /* validate input */
 		return (NULL);
 
+	/* find length to malloc */
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-			size++;
+		len += _strlen(av[i]);
 	}
+	len += (ac + 1); /* add space for newlines and null terminator */
 
-	size += (ac + 1);
-	s = malloc(sizeof(char) * size);
+	/* allocate memory and free if error */
+	s = malloc(len * sizeof(char));
+
 	if (s == NULL)
-		return (NULL);
-	k = 0;
-	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			s[k] = av[i][j];
-			k++;
-		}
-		s[k] = '\n';
-		k++;
+		free(s);
+		return (NULL);
 	}
 
-	s[k] = '\0';
+	/* insert each arg into *str */
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; j < _strlen(av[i]); j++)
+		{
+			s[k++] = av[i][j];
+		}
+		s[k++] = '\n';
+	}
 
 	return (s);
 }
