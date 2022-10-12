1c829409cff0ee27a4d6728b3b13ae1842db0740
Jason Uzoukwu <theweirdjason@gmail.com>
2022-10-12 12:55:24 -0700

    committed

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
-#include <stdio.h>
-#include "function_pointers.h"
+
 /**
- * print_name - print the name
- * @name: name to print
- * @f: function to use
+ * print_name - execute a function given as a parameter
+ * @name: parameter that function ptr needs
+ * @f:function to execute
+ *
  */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
