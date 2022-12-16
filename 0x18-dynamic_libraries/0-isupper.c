0f8fd9769eaa67c7c1c795e4cb05c8ccb6465e54
devchuks <111002665+devchuks@users.noreply.github.com>
2022-12-16 21:34:11 +0100

    add

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase checker
+ * @c: the letter
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+
+	return (0);
+}
